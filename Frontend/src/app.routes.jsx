a8c8c0b3439f35f83633a24b1fea81267374f91a
sameerraza9 <sameedakhtar9@gmail.com>
2026-05-07 22:57:33 +0500

    Folder Structure Maintainded

---
+++ Frontend/src/app.routes.jsx
@@ -0,0 +1,8 @@
+import {createBrowserRouter} from "react-router-dom";
+import Register from "./features/auth/pages/Register";
+import Login from "./features/auth/pages/Login";
+
+
+
+
+export const router = createBrowserRouter([])
