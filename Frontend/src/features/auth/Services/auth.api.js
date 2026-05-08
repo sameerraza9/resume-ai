be879e397c3dfef5e0fea37b5b72b103c598f909
sameerraza9 <sameedakhtar9@gmail.com>
2026-05-08 22:51:01 +0500

    Refactor auth API service for improved readability and consistency

--- Frontend/src/features/auth/Services/auth.api.js
+++ Frontend/src/features/auth/Services/auth.api.js
@@ -1,17 +1,32 @@
 import axios from "axios"
 
-export async function register({username, email, password}) {
-    
+
+
+export async function register({ username, email, password }) {
+
     try {
-        const response = await axios("http://localhost:3000/api/auth/register",{
-            username, email,password
-        },{
-            withCredentials : true
-        })
+        const response = await axios("http://localhost:3000/api/auth/register", {
+            username, email, password
+        }, { withCredentials: true })
 
         return response.data
+
     } catch (err) {
         console.log(err);
     }
 
+}
+
+
+export async function login({ username, password }) {
+    try {
+        const response = await axios.post("http://localhost:3000/api/auth/login", {
+            username, password
+        }, { withCredentials: true })
+
+        return response.data
+
+    } catch (err) {
+        console.log(err);
+    }
 }
