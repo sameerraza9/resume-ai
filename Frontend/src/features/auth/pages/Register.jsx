033f907c44239508d8d47243b68cb7249e268932
sameerraza9 <sameedakhtar9@gmail.com>
2026-05-18 21:36:28 +0500

    Refactor Register component to manage form state and handle registration logic

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import {useNavigate, Link} from "react-router"
+import React, { useState } from 'react'
+import { useNavigate, Link } from "react-router"
+import { useAuth } from '../hooks/useAuth'
 
 
 const Register = () => {
 
   const navigate = useNavigate()
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-  const handleSubmit = (e) => {
+  const { handleRegister, loading } = useAuth()
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
+    await handleRegister({ email, username, password })
+    navigate("/")
   }
+
+  if (loading) {
+    return (<main><h1>Loading...</h1></main>)
+  }
+
 
   return (
     <main>
@@ -19,23 +32,23 @@ const Register = () => {
 
           <div className="input-group">
             <label htmlFor="username">Username</label>
-            <input type="text" id='username' name='username' placeholder='Enter Username' />
+            <input type="text" onChange={(e) => { setUsername(e.target.value) }} id='username' name='username' placeholder='Enter Username' />
           </div>
 
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" id='email' name='email' placeholder='Enter Email' />
+            <input type="email" onChange={(e) => { setEmail(e.target.value) }} id='email' name='email' placeholder='Enter Email' />
           </div>
 
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id='password' name='password' placeholder='Enter Password' />
+            <input type="password" onChange={(e) => { e.target.value }} id='password' name='password' placeholder='Enter Password' />
           </div>
 
           <button className='button primary-button'>Register</button>
 
         </form>
-        
+
         <p>Already have an account? <Link to={"/login"}>Login</Link></p>
 
       </div>
