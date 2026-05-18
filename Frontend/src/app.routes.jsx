a2b5adecd84451e8a3bbd8ee3fe666f3047f8806
sameerraza9 <sameedakhtar9@gmail.com>
2026-05-18 22:22:23 +0500

    Add Protected component to manage access control and wrap Home Page

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Register from "./features/auth/pages/Register.jsx";
 import Login from "./features/auth/pages/Login.jsx";
+import Protected from "./features/auth/components/Protected.jsx";
 
 
 
@@ -16,6 +17,6 @@ export const router = createBrowserRouter([
     },
     {
         path: "/",
-        element: <h1>Home Page</h1>
+        element: <Protected><h1>Home Page</h1></Protected>
     }
 ])
