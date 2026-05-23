d8953b8fbe46dc474563f73d2f1cf25b6657922d
sameerraza9 <sameedakhtar9@gmail.com>
2026-05-23 21:45:43 +0500

    Add Google GenAI API key to environment variables and implement invokeGeminiAi function for content generation

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,15 @@ const app = require("./src/app.js")
 const connectToDB = require("./src/config/database.js")
 const dns = require("dns")
 dns.setServers(["0.0.0.0", "8.8.8.8"])
+const invokeGeminiAi = require("./src/services/ai.service.js")
+
+
+
+
 
 connectToDB()
+
+invokeGeminiAi()
 app.listen(3000, ()=>{
     console.log("server is listening on port 3000");
 })
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,6 +1,20 @@
-const {GoogleGenAI} = require('@google/generative-ai');
+const { GoogleGenAI } = require ("@google/generative-ai")
 
 
 const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_GENAI_API_KEY
 })
+
+
+async function invokeGeminiAi() {
+
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: "Hello Gemini! Explain what is interview?"
+    })
+
+    console.log(response.text);
+
+}
+
+module.exports = invokeGeminiAi
