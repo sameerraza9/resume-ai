fbb20c41c61e5fec18229cb8f233f3817d4f73a5
sameerraza9 <sameedakhtar9@gmail.com>
2026-05-27 22:56:18 +0500

    Home component layout; enhance styling for interview input group and add file upload label

--- Frontend/src/features/interview/pages/Home.jsx
+++ Frontend/src/features/interview/pages/Home.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
+import "../styles/home.scss"
+
 
 const Home = () => {
     return (
         <main className='home'>
-            <div className="left">
-                <textarea name="jobDescription" id="jobDescription" placeholder="Enter job description..."></textarea>
-            </div>
-            <div className="right">
-                <div className="input-group">
-                    <label htmlFor="resume">Upload Resume</label>
-                    <input type="file" id="resume" accept=".pdf" />
-                </div>
 
-                <div className="input-group">
-                    <label htmlFor="selfDescription">Upload Self Description</label>
-                    <textarea name="selfDescription" id="selfDescription" placeholder="Enter self description..."></textarea>
+            <div className="interview-input-group">
+                <div className="left">
+                    <textarea name="jobDescription" id="jobDescription" placeholder="Enter job description..."></textarea>
                 </div>
-                <button className='generate-btn'>Generate Interview Report</button>
+                <div className="right">
+                    <div className="input-group">
+                        <p>Resume <small className='highlight'>Use Resume and Self Description for better results</small></p>
+                        <label className='file-label' htmlFor="resume">Upload Resume</label>
+                        <input hidden type="file" id="resume" accept=".pdf" />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="selfDescription">Self Description</label>
+                        <textarea name="selfDescription" id="selfDescription" placeholder="Enter self description..."></textarea>
+                    </div>
+                    <button className='button primary-button btn'>Generate Interview Report</button>
+                </div>
             </div>
+
+
         </main>
     )
 }
